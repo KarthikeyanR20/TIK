1d09b7089594e0a85bc2b2e1907ea01049978902
Karthikeyan RKG <karthikeyanr@Karthikeyans-MacBook-Air.local>
2026-05-09 16:27:43 +0530

    layout changed

--- frontend/src/components/Task/Find_Jobs.js
+++ frontend/src/components/Task/Find_Jobs.js
@@ -6,28 +6,65 @@ import Logo3 from '../Images/JOBS/3.png'
 import Logo4 from '../Images/JOBS/4.png'
 import Logo5 from '../Images/JOBS/5.png'
 import Logo6 from '../Images/JOBS/6.png'
+import Logo7 from '../Images/JOBS/7.png'
+import Logo8 from '../Images/JOBS/8.png'
+import Logo9 from '../Images/JOBS/9.png'
+import Logo10 from '../Images/JOBS/10.png'
+import Logo11 from '../Images/JOBS/11.png'
+import Logo12 from '../Images/JOBS/12.png'
+import Logo13 from '../Images/JOBS/13.png'
+import Logo14 from '../Images/JOBS/14.png'
+import Logo15 from '../Images/JOBS/15.png'
+import Logo16 from '../Images/JOBS/16.png'
+import Logo17 from '../Images/JOBS/17.png'
+import Logo18 from '../Images/JOBS/18.png'
+import Logo19 from '../Images/JOBS/19.png'
+import Logo20 from '../Images/JOBS/20.png'
+import Logo21 from '../Images/JOBS/21.png'
+import Logo22 from '../Images/JOBS/22.png'
+import Logo23 from '../Images/JOBS/23.png'
+import Logo24 from '../Images/JOBS/24.png'
+import Logo25 from '../Images/JOBS/25.png'
+import Logo26 from '../Images/JOBS/26.png'
+import Logo27 from '../Images/JOBS/27.png'
+import Logo28 from '../Images/JOBS/28.png'
+import Logo29 from '../Images/JOBS/29.png'
+import Logo30 from '../Images/JOBS/30.png'
+
 import { Link } from 'react-router-dom'
 
 function Find_Jobs() {
     const jobs = [
-        { title: "Web Developer", logo: Logo1 },
-        { title: "Software Developer", logo: Logo2 },
-        { title: "Game Developer", logo: Logo3 },
-        { title: "Cyber Security", logo: Logo4 },
-        { title: "AIDS", logo: Logo5 },
-        { title: "HR Manager", logo: Logo6 },
-        { title: "Web Developer", logo: Logo1 },
-        { title: "Software Developer", logo: Logo2 },
-        { title: "Game Developer", logo: Logo3 },
-        { title: "Cyber Security", logo: Logo4 },
-        { title: "AIDS", logo: Logo5 },
-        { title: "HR Manager", logo: Logo6 },
-        { title: "Web Developer", logo: Logo1 },
-        { title: "Software Developer", logo: Logo2 },
-        { title: "Game Developer", logo: Logo3 },
-        { title: "Cyber Security", logo: Logo4 },
-        { title: "AIDS", logo: Logo5 },
-        { title: "HR Manager", logo: Logo6 },
+        { title: "Software Development", logo: Logo1 },
+        { title: "Data Science", logo: Logo2 },
+        { title: "Cyber Security", logo: Logo3 },
+        { title: "Marine Engineer", logo: Logo4 },
+        { title: "Cloud & Devops", logo: Logo5 },
+        { title: "Port Operations", logo: Logo6 },
+        { title: "Logistics & Freight Handling", logo: Logo7 },
+        { title: "Captain & Deck Officer", logo: Logo8 },
+        { title: "HR Operations", logo: Logo9 },
+        { title: "Reqruiter (Talent Acquisition)", logo: Logo10 },
+        { title: "Manufacturing", logo: Logo11 },
+        { title: "Civil Engineering", logo: Logo12 },
+        { title: "Mechanical Engineering", logo: Logo13 },
+        { title: "Production and Quality", logo: Logo14 },
+        { title: "Maintenance", logo: Logo15 },
+        { title: "Voice Process (Domestic & Intenational)", logo: Logo16 },
+        { title: "Non-Voice Process", logo: Logo17 },
+        { title: "Technical Support", logo: Logo18 },
+        { title: "Desktop Support Engineer", logo: Logo19 },
+        { title: "Network Engineer", logo: Logo20 },
+        { title: "System Administrator", logo: Logo21 },
+        { title: "Hardware & Infrastructure Support", logo: Logo22 },
+        { title: "Ground Staff", logo: Logo23 },
+        { title: "Cabin Crew", logo: Logo24 },
+        { title: "Cargo Handling", logo: Logo25 },
+        { title: "Oversea Jobs (UK, Europe, Germany, Canada, Australia & Gulf)", logo: Logo26 },
+        { title: "Nursing Overseas", logo: Logo27 },
+        { title: "Banking & Finance", logo: Logo28 },
+        { title: "Warehouse Executive", logo: Logo29 },
+        { title: "Railways Private Jobs", logo: Logo30 }
     ]
     return (
         <>
