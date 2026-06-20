6e402bfbaeea09258c33a8556717c1f6f4aba8a7
Karthikeyan RKG <karthikeyanr@Karthikeyans-MacBook-Air.local>
2026-06-20 20:38:40 +0530

    “changes”

--- frontend/src/components/Pages/ProtectedRoute.jsx
+++ frontend/src/components/Pages/ProtectedRoute.jsx
@@ -5,7 +5,7 @@ function ProtectedRoute({ children }) {
   const token = localStorage.getItem("adminToken");
 
   if (!token) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/admin-login" replace />;
   }
 
   return children;
